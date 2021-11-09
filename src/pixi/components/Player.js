1036f1c6f8a135f8eb60e26d449cacfe2557f7df
LIYI <liyibass@gmail.com>
2021-11-09 20:21:57 +0800

    feat: implement dragging function

--- src/pixi/components/Player.js
+++ src/pixi/components/Player.js
@@ -6,6 +6,8 @@ export class Player {
     this.createSprite()
     this.sprite.x = position.x
     this.sprite.y = position.y
+    this.setInteractive()
+    this.dragging = false
   }
 
   createSprite() {
@@ -13,5 +15,36 @@ export class Player {
 
     this.sprite = new PIXI.Sprite(texture)
     this.sprite.anchor.set(0.5, 0.5)
+  }
+
+  setInteractive() {
+    this.sprite.interactive = true
+
+    this.sprite.on('mousedown', this.onTouchStart.bind(this))
+    this.sprite.on('mousemove', this.onTouchMove.bind(this))
+  }
+
+  onTouchStart(event) {
+    // 1. remember the position of the mouse cursor
+    this.touchPosition = {
+      x: event.data.global.x,
+      y: event.data.global.y,
+    }
+
+    // 2.set the dragging state for this sprite
+    this.dragging = !this.dragging
+  }
+  onTouchMove(event) {
+    if (!this.dragging) return
+
+    // 1. get the corrdinates of ther cursor
+    const currentPosition = {
+      x: event.data.global.x,
+      y: event.data.global.y,
+    }
+
+    // 3. apply the rusulting offset
+    this.sprite.x = currentPosition.x
+    this.sprite.y = currentPosition.y
   }
 }
